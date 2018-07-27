ebd508df5595f3246ce0cfa680d50adb1dfcb78a
ulihgu <ulihgu@msn.com>
2018-07-27 21:23:47 +0800

    2018-7-26-1

--- js/npm.js
+++ js/npm.js
@@ -5,4 +5,19 @@ function displayDate(){
 function toolu(){
 	alert("天才的开始！");
 }
-toolu();
+
+function myFunction(){
+	x=document.getElementById("demo");
+	x.innerHTML="这是改变后的内容";
+}
+
+function changeImage(){
+	Element=document.getElementById("myimage")
+	if(Element.src.match("bulbon"))
+	{
+		Element.src="img/pic_bulboff.gif";
+	}
+	else{
+		Element.src="img/pic_bulbon.gif";
+	}
+}
